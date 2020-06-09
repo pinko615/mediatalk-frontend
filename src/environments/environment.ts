8e7090b797db00e5690441d16f03f61e5de03087
pinko615 <martin.pinto@hotmail.com>
2020-06-09 14:35:43 +0200

    enviroment apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseUrl: window.location.origin,
+  apiUrl: 'http://localhost:8000/api'
 };
 
 /*
