9d915c341916440e6adc59ca79a4cb39cc19581f
pinko615 <martin.pinto@hotmail.com>
2020-06-09 14:42:19 +0200

    guest navigation updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './_components/landing/landing.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'landing',
+    component: LandingComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './_components/landing/landing.component';
+import { AuthNavComponent } from './_components/navigation/auth-nav/auth-nav.component';
+import { GuestNavComponent } from './_components/navigation/guest-nav/guest-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    AuthNavComponent,
+    GuestNavComponent
   ],
   imports: [
     BrowserModule,
